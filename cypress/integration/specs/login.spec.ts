58c8a2b1caacb62bee08e168b9a04aac11aae1f0
mikhail <zig-92@ya.ru>
2022-04-07 11:54:50 +0800

    updated login test

--- cypress/integration/specs/login.spec.ts
+++ cypress/integration/specs/login.spec.ts
@@ -5,7 +5,7 @@ describe('Registration page', () => {
     const password = "qweQWE!@#123";
     const invalidPassword = "qweQWE";
     const email = "zig-92@ya.ru";
-    const invalidEmail = "zig.ksdf";
+    const invalidEmail = "zig-92@gh.ksdf";
 
     beforeEach(() => {
         cy.log('Visiting login for ' + workspace + ' workspace..');
@@ -15,12 +15,14 @@ describe('Registration page', () => {
     it('Visit login page, login fail with invalid password', () => {
         login.typeEmail(email);
         login.typePassword(invalidPassword);
+        login.clickLogin();
         login.getInvalidCredsLabel().should("be.visible");
     });
 
     it('Visit login page, login fail with invalid email', () => {
         login.typeEmail(invalidEmail);
         login.typePassword(password);
+        login.clickLogin();
         login.getInvalidCredsLabel().should("be.visible");
     });
 
